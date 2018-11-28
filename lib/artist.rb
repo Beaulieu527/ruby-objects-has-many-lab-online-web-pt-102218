1bf69d651a415a6b32507d17ec7b30c2f6cfb252
Justin Beaulieu <beaulieudev@gmail.com>
2018-11-28 01:11:20 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,19 +7,4 @@ class Artist
     @songs = []
   end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-
-  def add_song_by_name(name)
-   song = Song.new(name)
-   @songs << song
-   song.artist = self
-  end
-
-  def self.song_count
-    @songs.length
-  end
-
 end
