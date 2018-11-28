a169ea300978162223d1066be25e25b0f3137190
Justin Beaulieu <beaulieudev@gmail.com>
2018-11-28 00:33:37 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist
+
+  attr_accessor :name
+
+  def intialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song
+
+  end
+
+  def add_song_by_name
+  end
+
+  def song_count
+    @songs.length
+  end
+
+end
