35a6ba7d99b7da20df711b1e545d8861848248ee
Justin Beaulieu <beaulieudev@gmail.com>
2018-11-28 01:29:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,24 @@
 class Artist
 
   attr_accessor :name
+  @@songs = []
 
   def initialize(name)
     @name = name
   end
 
   def songs
-    @song = []
+    @@song
   end
 
   def add_song(song)
-    @songs << song
+    @@songs << song
     song.artist = self
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
+    @@songs << song
     song.artist = self
   end
 
